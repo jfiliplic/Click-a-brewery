2a655cacc10a6cca3cfa861c67a520b8f4b1331a
jfiliplic <97060179+jfiliplic@users.noreply.github.com>
2023-04-23 00:14:34 +0200

    updated js for card to display random bg images

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,6 @@
 const btn = document.querySelector(".getBrewery");
 const breweryHolder = document.querySelector(".brewery p");
-// const breweryCardBackground = document.querySelector(".wrapper").style;
-// console.log(breweryCardBackground);
-// const breweryCardBackground = breweryCard.style.background;
-const breweryCard = document.querySelector(".wrapper");
-const breweryCardStyle = breweryCard.getAttribute("style");
-// const setImage = breweryCard.setAttribute(
-//   "style",
-//   `backgroundImage: url(../assets/images/bw-bg.jpg)`
-// );
-// console.log(breweryCardStyle);
-// breweryCard.setAttribute(
-//   "style",
-//   `
-// background: black;
-//     `
-// );
+const cardStyle = document.querySelector(".wrapper").style;
 
 const btnText = [
   "Good one",
@@ -26,6 +11,15 @@ const btnText = [
   "Not in a million years!",
   "I need a beer!",
   "Let's have a pint!",
+];
+
+const cardImages = [
+  `url("beer2.246afe6f.jpg")`,
+  `url("beer3.96ca2431.jpg")`,
+  `url("beer4.0bf99960.jpg")`,
+  `url("beer5.0ee91788.jpg")`,
+  `url("beer6.070a6985.jpg")`,
+  `url("beer7.74872d00.jpg")`,
 ];
 
 async function fetchBrewery() {
@@ -44,66 +38,25 @@ async function handleClick() {
   breweryHolder.textContent = `${name}\r\n${city}\r\n${country}\r\n${
     website_url || "no website available"
   }`;
-  //   breweryHolder.textContent += `${city}\r\n`;
-  //   breweryHolder.textContent += `${country}`;
   btn.textContent = randomItemFromArray(btnText, btn.textContent);
-
-  // to zdaj dela
-  //   breweryCard.style["background-image"] = `${randomImageFromPicsum(
-  //     breweryCard.style["background-image"]
-  //   )}`;
-  console.log(breweryCard.style["background-image"]);
-  breweryCard.style["background-image"] = "url(./beer-1.jpg)";
-  console.log(breweryCard.style["background-image"]);
-
-  // različni poskusi
-  //   breweryCard.style["background-image"] = `url("$s{randomImageFromPicsum(
-  //     breweryCard.style["background-image"]
-  //   )}")`;
-  //   console.log(breweryCard.style["background-image"]);
-  //   breweryCard.style.backgroundImage = "url(`./beer-1.jpg`)";
-  //   breweryCard.style[
+  cardStyle["background-image"] = randomItemFromArray(
+    cardImages,
+    `${cardStyle["background-image"]}`
+  );
+  //   cardStyle[
   //     "background-image"
-  //   ] = `url("https://picsum.photos/350/500?grayscale&random=${Math.floor(
-  //     Math.random() * 100
+  //   ] = `url("https://picsum.photos/900/600?grayscale&random=${Math.floor(
+  //     Math.random() * 50
   //   )}")`;
-  //   breweryCard.style.color = "blue";
-  //   breweryCard.setAttribute(
-  //     "style",
-  //     `
-  //       background-image: url("https://picsum.photos/200/300");
-  //       color: purple;
-  //       `
-  //   );
-  //   breweryCardBackground = randomImageFromAssets(breweryCardBackground);
-  //   breweryCard.setAttribute(
-  //     "style",
-  //     `
-  //   background: black;
-  //       `
-  //   );
 }
 
 function randomItemFromArray(arr, not) {
   const item = arr[Math.floor(Math.random() * arr.length)];
   if (item === not) {
+    console.log(not);
     return randomItemFromArray(arr, not);
   }
   return item;
 }
-
-//to zdaj dela- ali sploh ni potrebno (že link poskrbi za pravi random?)?
-// function randomImageFromPicsum(not) {
-//   // const image = `../assets/images/random${Math.floor(Math.random()) * 2}.jpg`;
-//   const image = `url("https://picsum.photos/200/300?grayscale&random=${Math.floor(
-//     Math.random() * 5
-//   )}")`;
-//   if (image === not) {
-//     console.log(not);
-//     return randomImageFromPicsum(not);
-//   }
-//   console.log(image);
-//   return image;
-// }
 
 btn.addEventListener("click", handleClick);
